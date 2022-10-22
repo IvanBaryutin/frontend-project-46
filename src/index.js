4005d5726e60fb9402ebba395ed8cdf117320074
Ivan Baryutin <vaness97@gmail.com>
2022-10-22 15:14:42 +0300

    Add: черновой вариант функции сравнения файлов

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-console.log(1111);
 import fs from 'fs';
 import _ from 'lodash';
 
@@ -14,33 +13,37 @@ const filePath2 = '/file2.json';
 const data1 = JSON.parse(readFile(filePath1));
 const data2 = JSON.parse(readFile(filePath2));
 
-console.log(data1);
-console.log(data2);
+console.log(data1.host);
+console.log(data2.host);
 
 
+const keys1 = Object.keys(data1);
+const keys2 = Object.keys(data2);
 
-/*
-const file1 = 
-{
-  "host": "hexlet.io",
-  "timeout": 50,
-  "proxy": "123.234.53.22",
-  "follow": false
-}
+const sortedKeys = _.union(keys1, keys2).sort();
 
-const file2 = 
-{
-  "timeout": 20,
-  "verbose": true,
-  "host": "hexlet.io"
-}
+let res = '{\n';
 
-const gendiff = (file1, file2) => {
-    //const obj1 = JSON.parse(file1);
-    //const obj2 = JSON.parse(file2);
-    //console.log(JSON.parse(file1));
-    console.log(JSON.parse(file2));        
-};
-*/
+sortedKeys.map((key) => {
+  // console.log(data1[key]);
+  let str = '';
+  if (key in data1 === false) {
+    str = ` + ${key}: ${data2[key]}\n`;
+  }
+  if (key in data1) {
+    if (data1[key] === data2[key]) {
+      str = `   ${key}: ${data2[key]}\n`;
+    } else {
+      if (key in data2 === false) {
+        str = ` - ${key}: ${data1[key]}\n`;
+      } else {
+        str = ` - ${key}: ${data1[key]}\n + ${key}: ${data2[key]}\n`;
+      }
+    }
+  }
+  res += str;
+});
 
-//gendiff(file1, file2);
+res += '}';
+
+console.log(res);
