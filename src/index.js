60fd74738950c97872236c1dbea9e4dbe969ac34
Ivan Baryutin <vanessru@yandex.ru>
2022-11-17 18:15:31 +0300

    Add: add makeAST func draft

--- src/index.js
+++ src/index.js
@@ -6,12 +6,63 @@ import parseData from './parsers.js';
 const readFile = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8');
 const getFileExt = (filename) => filename.split('.').pop();
 
+const makeAST = (oldData, newData) => {
+  const oldKeys = Object.keys(oldData);
+  const newKeys = Object.keys(newData);
+  const sortedKeys = _.union(oldKeys, newKeys).sort();
+
+  const result = sortedKeys.map((key) => {
+    // Нет ключа в исходной структуре: добавлено
+    if (key in oldData === false) {
+      return {
+        key,
+        type: 'added',
+        value: newData[key],
+      };
+    }
+    // Нет ключа в новой структуре: удалено
+    if (key in newData === false) {
+      return {
+        key,
+        type: 'removed',
+        value: oldData[key],
+      };
+    }
+    // Значения по ключу отличаются
+    if (oldData[key] !== newData[key]) {
+      return {
+        key,
+        type: 'modified',
+        firstValue: oldData[key],
+        secondValue: newData[key],
+      };
+    }
+    // Значение одного и того же ключа в обеих структурах — объект, строим children
+    if (typeof oldData[key] === 'object' && typeof newData[key] === 'object') {
+      return {
+        key,
+        type: 'nested',
+        children: makeAST(oldData[key], newData[key]),
+      };
+    }
+    // Иначе значение не менялось, добавляем как есть.
+    return {
+      key,
+      type: 'notModified',
+      value: oldData[key],
+    };
+  });
+  return result;
+};
+
 const genDiff = (oldFile, newFile) => {
   // Парсим файлы
   const oldData = parseData(readFile(oldFile), getFileExt(oldFile));
   // console.log(oldData);
   const newData = parseData(readFile(newFile), getFileExt(newFile));
   // console.log(newData);
+
+  console.log(makeAST(oldData, newData));
 
   // Берем собираем ключи
   const oldKeys = Object.keys(oldData);
